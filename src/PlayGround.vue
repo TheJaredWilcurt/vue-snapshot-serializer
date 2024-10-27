07ac79f12f9c9bed2d58a3197d134ef66bead9b9
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-10-27 19:33:49 -0400

    Playground has all options

--- src/PlayGround.vue
+++ src/PlayGround.vue
@@ -1,55 +1,79 @@
 <template>
   <div class="playground">
     <aside class="playground-controls">
-      <label>
-        Font size ({{ fontSize }}%)
-        <input v-model.number="fontSize" type="range" min="50" max="200">
-      </label>
-      <label>
-        <input v-model="stacked" type="checkbox">
-        Stacked
-      </label>
-      <label>
-        Formatter:
-        <select v-model="vueSnapshots.formatter">
-          <option value="none">None</option>
-          <option value="diffable">Diffable</option>
-          <option value="custom">Custom Function</option>
-        </select>
-      </label>
+      <fieldset>
+        <label>
+          Font size ({{ fontSize }}%)
+          <input v-model.number="fontSize" type="range" min="50" max="200">
+        </label>
+      </fieldset>
+      <DoxenCheckbox
+        v-model="stacked"
+        name="Stacked"
+      />
+      <DoxenCheckbox
+        v-for="(value, key) in TOP_LEVEL_BOOLEANS"
+        v-model="vueSnapshots[key]"
+        :name="value"
+      />
+      <p><strong>*</strong> Requires a Vue-Test-Utils wrapper, not HTML</p>
+      <fieldset>
+        <label>
+          Attributes to clear:
+          <input v-model="vueSnapshots.attributesToClear" placeholder="Comma separated list of attributes">
+        </label>
+      </fieldset>
+      <fieldset>
+        <label>
+          Formatter:
+          <select v-model="vueSnapshots.formatter">
+            <option value="none">None</option>
+            <option value="diffable">Diffable</option>
+            <option value="custom">Custom Function</option>
+          </select>
+        </label>
+      </fieldset>
       <template v-if="vueSnapshots.formatter === 'diffable'">
-        <label>
-          Void Elements:
-          <select v-model="vueSnapshots.formatting.voidElements">
-            <option value="html">&lt;input&gt;</option>
-            <option value="xhtml">&lt;input /&gt;</option>
-            <option value="closingTag">&lt;input&gt;&lt;/input&gt;</option>
-          </select>
-        </label>
-        <label>
-          <input v-model="vueSnapshots.formatting.emptyAttributes" type="checkbox">
-          Show Empty Attributes
-        </label>
-        <label>
-          <input v-model="vueSnapshots.formatting.selfClosingTag" type="checkbox">
-          Non-Void Self Closing Tags
-        </label>
-        <label>
-          Attributes Per Line:
-          <input v-model.number="vueSnapshots.formatting.attributesPerLine" type="number" min="0">
-        </label>
-        <label>
-          Preserve Whitespace in Tags:
-          <select v-model="vueSnapshots.formatting.tagsWithWhitespacePreserved">
-            <option :value="true">All tags</option>
-            <option value="custom">Specific tags</option>
-            <option :value="false">No tags</option>
-          </select>
-        </label>
-        <label v-if="vueSnapshots.formatting.tagsWithWhitespacePreserved === 'custom'">
-          Tags to preserve whitespace in:
-          <input v-model="whitespaceTagsList" placeholder="Comma separate list">
-        </label>
+        <fieldset>
+          <label>
+            Void Elements:
+            <select v-model="vueSnapshots.formatting.voidElements">
+              <option value="html">&lt;input&gt;</option>
+              <option value="xhtml">&lt;input /&gt;</option>
+              <option value="closingTag">&lt;input&gt;&lt;/input&gt;</option>
+            </select>
+          </label>
+        </fieldset>
+        <DoxenCheckbox
+          v-model="vueSnapshots.formatting.emptyAttributes"
+          name="Show Empty Attributes"
+        />
+        <DoxenCheckbox
+          v-model="vueSnapshots.formatting.selfClosingTag"
+          name="Non-Void Self Closing Tags"
+        />
+        <fieldset>
+          <label>
+            Attributes Per Line:
+            <input v-model.number="vueSnapshots.formatting.attributesPerLine" type="number" min="0">
+          </label>
+        </fieldset>
+        <fieldset>
+          <label>
+            Preserve Whitespace in Tags:
+            <select v-model="vueSnapshots.formatting.tagsWithWhitespacePreserved">
+              <option :value="true">All tags</option>
+              <option value="custom">Specific tags</option>
+              <option :value="false">No tags</option>
+            </select>
+          </label>
+        </fieldset>
+        <fieldset>
+          <label v-if="vueSnapshots.formatting.tagsWithWhitespacePreserved === 'custom'">
+            Tags to preserve whitespace in:
+            <input v-model="whitespaceTagsList" placeholder="Comma separated list of tags">
+          </label>
+        </fieldset>
       </template>
     </aside>
     <main class="playground-content">
@@ -79,21 +103,16 @@
 </template>
 
 <script>
-import { DoxenCodeBox } from 'vue-doxen';
+import _xor from 'lodash.xor';
+import {
+  DoxenCheckbox,
+  DoxenCodeBox
+} from 'vue-doxen';
 import { vueMarkupFormatter } from 'vue3-snapshot-serializer';
 
-const defaults = {
-  attributesPerLine: 1,
-  emptyAttributes: true,
-  formatter: 'diffable',
-  selfClosingTag: false,
-  tagsWithWhitespacePreserved: ['a', 'pre'],
-  voidElements: 'xhtml'
-};
-
 const exampleCode = `
-<div id="header">
-  <label>
+<div id="header" data-server-rendered>
+  <label data-test="input" data-v-1ae75a9f="">
     Void and Attributes per line Example:
     <input>
     <input type="range">
@@ -106,10 +125,57 @@ const exampleCode = `
 </div>
 `.trim();
 
+const defaults = Object.freeze({
+  addInputValues: true,
+  attributesPerLine: 1,
+  attributesToClear: [],
+  clearInlineFunctions: false,
+  emptyAttributes: true,
+  formatter: 'diffable',
+  removeComments: false,
+  removeServerRendered: true,
+  removeDataVId: true,
+  removeDataTest: true,
+  removeDataTestid: true,
+  removeDataTestId: true,
+  removeDataQa: false,
+  removeDataCy: false,
+  removeDataPw: false,
+  removeIdTest: false,
+  removeClassTest: false,
+  selfClosingTag: false,
+  sortAttributes: true,
+  stringifyAttributes: true,
+  tagsWithWhitespacePreserved: ['a', 'pre'],
+  verbose: true,
+  voidElements: 'xhtml'
+});
+
 export default {
   name: 'PlayGround',
   components: {
+    DoxenCheckbox,
     DoxenCodeBox
+  },
+  constants: {
+    TOP_LEVEL_BOOLEANS: {
+      verbose: 'Verbose',
+      removeServerRendered: 'Remove data-server-rendered',
+      removeComments: 'Remove Comments',
+      removeDataVId: 'Remove data-v-...',
+      removeDataTest: 'Remove data-test',
+      removeDataTestid: 'Remove data-testid',
+      removeDataTestId: 'Remove data-test-id',
+      removeDataQa: 'Remove data-qa',
+      removeDataCy: 'Remove data-cy',
+      removeDataPw: 'Remove data-pw',
+      removeIdTest: 'Remove id="test..."',
+      removeClassTest: 'Remove class="test..."',
+      clearInlineFunctions: 'Clear inline functions',
+      sortAttributes: 'Sort Attributes',
+      addInputValues: 'Add Input Values*',
+      stringifyAttributes: 'Stringify Attributes*'
+    }
   },
   data: function () {
     return {
@@ -118,13 +184,30 @@ export default {
       stacked: false,
       whitespaceTagsList: 'a, pre',
       vueSnapshots: {
-        formatter: 'diffable',
+        addInputValues: defaults.addInputValues,
+        attributesToClear: '',
+        stringifyAttributes: defaults.stringifyAttributes,
+        clearInlineFunctions: defaults.clearInlineFunctions,
+        formatter: defaults.formatter,
+        removeClassTest: defaults.removeClassTest,
+        removeComments: defaults.removeComments,
+        removeDataCy: defaults.removeDataCy,
+        removeDataPw: defaults.removeDataPw,
+        removeDataQa: defaults.removeDataQa,
+        removeDataTest: defaults.removeDataTest,
+        removeDataTestid: defaults.removeDataTestid,
+        removeDataTestId: defaults.removeDataTestId,
+        removeDataVId: defaults.removeDataVId,
+        removeIdTest: defaults.removeIdTest,
+        removeServerRendered: defaults.removeServerRendered,
+        sortAttributes: defaults.sortAttributes,
+        verbose: defaults.verbose,
         formatting: {
-          attributesPerLine: 1,
-          emptyAttributes: true,
-          selfClosingTag: false,
+          attributesPerLine: defaults.attributesPerLine,
+          emptyAttributes: defaults.emptyAttributes,
+          selfClosingTag: defaults.selfClosingTag,
           tagsWithWhitespacePreserved: 'custom',
-          voidElements: 'xhtml'
+          voidElements: defaults.voidElements
         }
       }
     };
@@ -138,12 +221,13 @@ export default {
         })
         .filter(Boolean)));
     },
-    diffableOptions: function () {
-      const settings = { ...this.vueSnapshots.formatting };
-      if (this.vueSnapshots.formatting.tagsWithWhitespacePreserved === 'custom') {
-        settings.tagsWithWhitespacePreserved = this.whitespaceTags;
-      }
-      return settings;
+    clearableAttributes: function () {
+      return Array.from(new Set(this.vueSnapshots.attributesToClear
+        .split(',')
+        .map((attribute) => {
+          return attribute.trim();
+        })
+        .filter(Boolean)));
     },
     printableSettings: function () {
       let snapshotSettings = {};
@@ -176,15 +260,22 @@ export default {
         } else if (!this.whitespaceTags.length) {
           setFormattingObject();
           snapshotSettings.formatting.tagsWithWhitespacePreserved = false;
-        } else if (
-          this.whitespaceTags.length === 2 &&
-          defaults.tagsWithWhitespacePreserved.includes(this.whitespaceTags[0]) &&
-          defaults.tagsWithWhitespacePreserved.includes(this.whitespaceTags[1])
-        ) {
+        } else if (_xor(this.whitespaceTags, defaults.tagsWithWhitespacePreserved).length) {
           setFormattingObject();
           snapshotSettings.formatting.tagsWithWhitespacePreserved = this.whitespaceTags;
         }
       }
+
+      Object.keys(this.TOP_LEVEL_BOOLEANS).forEach((bool) => {
+        if (this.vueSnapshots[bool] !== defaults[bool]) {
+          snapshotSettings[bool] = this.vueSnapshots[bool];
+        }
+      });
+
+      if (this.clearableAttributes.length) {
+        snapshotSettings.attributesToClear = this.clearableAttributes;
+      }
+
       snapshotSettings = JSON
         .stringify(snapshotSettings, null, 2)
         .split('\n')
@@ -198,38 +289,40 @@ export default {
         .join('\n')
         .replace(
           '"custom"',
-          'function (markup) {\n    return markup.toUpperCase();\n  }'
+          [
+            'function (markup) {',
+            '    // Your custom logic to format the markup',
+            '    return markup.toUpperCase();',
+            '  }'
+          ].join('\n')
         )
         .replaceAll('"', '\'');
-      snapshotSettings = 'global.vueSnapshots = ' + snapshotSettings;
+      snapshotSettings = 'globalThis.vueSnapshots = ' + snapshotSettings + ';';
       snapshotSettings = '// Based on your settings (excluding those that match the defaults)\n' + snapshotSettings;
       return snapshotSettings;
     },
     size: function () {
       return 'font-size: ' + this.fontSize + '%';
     },
-    styleTokens: function () {
-      return {
-        codeBoxContainer: '',
-        codeBox: ''
-      };
-    },
     output: function () {
-      let markup = this.input;
+      window.vueSnapshots = {
+        ...this.vueSnapshots,
+        attributesToClear: this.clearableAttributes,
+        formatting: undefined
+      };
       if (this.vueSnapshots.formatter === 'diffable') {
-        window.vueSnapshots = {
-          formatter: 'diffable',
-          formatting: {
-            ...this.diffableOptions
-          }
+        window.vueSnapshots.formatting = {
+          ...this.vueSnapshots.formatting
+        }
+        if (this.vueSnapshots.formatting.tagsWithWhitespacePreserved === 'custom') {
+          window.vueSnapshots.formatting.tagsWithWhitespacePreserved = this.whitespaceTags;
+        }
+      } else if (this.vueSnapshots.formatter === 'custom') {
+        window.vueSnapshots.formatter = function (markup) {
+          return markup.toUpperCase();
         };
-        markup = vueMarkupFormatter(markup);
       }
-      if (this.vueSnapshots.formatter === 'custom') {
-        markup = markup.toUpperCase();
-      }
-      const language = 'html';
-      return markup;
+      return vueMarkupFormatter(this.input);
     }
   }
 };
@@ -247,8 +340,17 @@ export default {
   flex-wrap: wrap;
   width: 300px;
 }
+.playground-controls fieldset {
+  border: 0px;
+  margin: 3px 10px 0px 10px;
+  padding: 0px;
+}
 .playground-controls label {
-  margin: 10px 10px 0px 10px;
+  margin: 10px 10px 0px 4px;
+}
+.playground-controls p {
+  margin: 3px 10px 0px 20px;
+  font-size: 90%;
 }
 .playground-content {
   width: 100%;
