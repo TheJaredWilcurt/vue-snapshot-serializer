1e2eca1b086b7bb6af6fe3d9bc0cdf79d6a81332
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-10-28 10:39:05 -0400

    Highlight JS Update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import constantsPlugin from 'vue-options-api-constants-plugin';
 import App from '@/App.vue';
 
 import 'normalize.css/normalize.css';
-import 'highlightjs/styles/tomorrow-night-bright.css';
+import '@highlightjs/cdn-assets/styles/tomorrow-night-bright.css';
 import '@/styles/fonts.js';
 import '@/styles/main.css';
 
